b02145422aaaf6ade24575fc830eca102e77ffa7
M Sravan kumar <129173155+MSK9493@users.noreply.github.com>
2024-12-02 21:57:13 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+      if(this.scrollY > 20){
+        $(".navbar").addClass("sticky");
+        $(".goTop").fadeIn();
+      }
+      else{
+        $(".navbar").removeClass("sticky");
+        $(".goTop").fadeOut();
+      }
+    });
+  
+    $(".goTop").click(function(){scroll(0,0)});
+  
+    $('.menu-toggler').click(function(){
+      $(this).toggleClass("active");
+      $(".navbar-menu").toggleClass("active");
+    });
+  
+    $(".works").magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      gallery:{enabled:true}
+    });
+  });
+  
+  
+  let slideIndex = 1;
+  
+  function showSlides(n) {
+      let slides = document.getElementsByClassName("slide");
+  
+      if (n > slides.length) {
+          slideIndex = 1;
+      }
+      if (n < 1) {
+          slideIndex = slides.length;
+      }
+  
+      for (let i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";
+      }
+  
+      slides[slideIndex - 1].style.display = "block";
+  }
+  
+  function plusSlides(n) {
+      showSlides(slideIndex += n);
+  }
+  
+  showSlides(slideIndex);
+  
